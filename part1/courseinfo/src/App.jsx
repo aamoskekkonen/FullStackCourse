c28e156b03d302ddaf653b4e3f7554222487a73b
aamoskekkonen <aamoskekkonen@icloud.com>
2023-10-18 12:35:48 +0300

    complete exercise 1.1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,40 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+  return (
+    <div>
+      <Header course={course} />
+      <Content exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+    </div>
+  )
+}
+
+const Header = ( {course} ) => {
+  return (<h1>{course}</h1>)
+}
+
+const Content = ( {exercises1, exercises2, exercises3} ) => {
+  const part1 = 'Fundamentals of React'
+  const part2 = 'Using props to pass data'
+  const part3 = 'State of a component'
+  return (
+    <>
+      <p>{part1} {exercises1}</p>
+      <p>{part2} {exercises2}</p>
+      <p>{part3} {exercises3}</p>
+    </>
+  )
+}
+
+const Total = ( {exercises1, exercises2, exercises3} ) => {
+  return (
+    <>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </>
+  )
+}
+
+export default App
