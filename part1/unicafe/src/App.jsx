573a0a61432501af457603cdc88272e98df1ba2d
aamoskekkonen <aamoskekkonen@icloud.com>
2023-10-19 11:58:23 +0300

    complete exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,32 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  return (
+    <div>
+      <Header text={'give feedback'} />
+      <div>
+        <Button text={'good'} value={good} setFunc={setGood}/>
+        <Button text={'neutral'} value={neutral} setFunc={setNeutral}/>
+        <Button text={'bad'} value={bad} setFunc={setBad}/>
+      </div>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+const Header = ( {text} ) => {
+  return (<h1>{text}</h1>)
+}
+
+const Button = ( {text, value, setFunc} ) => {
+  return (<button onClick={() => setFunc(value + 1)}>{text}</button>)
+}
+
+const StatisticsComponent = ( {text, value} ) => {
+  return (<p>{text} {value}</p>)
+}
+
+const Statistics = ( {good, neutral, bad} ) => {
   const average = () => {
     const goodValue = good * 1
     const neutralValue = neutral * 0
@@ -24,32 +50,14 @@ const App = () => {
 
   return (
     <div>
-      <Header text={'give feedback'} />
-      <div>
-        <Button text={'good'} value={good} setFunc={setGood}/>
-        <Button text={'neutral'} value={neutral} setFunc={setNeutral}/>
-        <Button text={'bad'} value={bad} setFunc={setBad}/>
-      </div>
       <Header text={'statistics'} />
-      <Statistic text={'good'} value={good}/>
-      <Statistic text={'neutral'} value={neutral}/>
-      <Statistic text={'bad'} value={bad}/>
-      <Statistic text={'average'} value={average()} />
-      <Statistic text={'positives'} value={positives()} />
+      <StatisticsComponent text={'good'} value={good}/>
+      <StatisticsComponent text={'neutral'} value={neutral}/>
+      <StatisticsComponent text={'bad'} value={bad}/>
+      <StatisticsComponent text={'average'} value={average()} />
+      <StatisticsComponent text={'positives'} value={positives()} />
     </div>
   )
-}
-
-const Header = ( {text} ) => {
-  return (<h1>{text}</h1>)
-}
-
-const Button = ( {text, value, setFunc} ) => {
-  return (<button onClick={() => setFunc(value + 1)}>{text}</button>)
-}
-
-const Statistic = ( {text, value} ) => {
-  return (<p>{text} {value}</p>)
 }
 
 export default App
