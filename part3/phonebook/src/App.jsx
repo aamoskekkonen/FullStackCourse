50f13781567a9b0e4f3330170ea5d9e89cf7cf73
Aamos Kekkonen <aamos.kekkonen@aalto.fi>
2024-01-08 08:00:07 +0200

    fix a typo

--- part3/phonebook/src/App.jsx
+++ part3/phonebook/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import SearchFilter from './components/SearchFilter'
 import AddingForm from './components/AddingForm'
 import Phonebook from './components/Phonebook'
